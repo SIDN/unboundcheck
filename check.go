bacdf8037688c804be562f2beb2043ae70149e0d
Miek Gieben <miek@miek.nl>
2012-07-09 12:58:15 +0200

    more stuff

--- check.go
+++ check.go
@@ -2,17 +2,69 @@ package main
 
 import (
 	"code.google.com/p/gorilla/mux"
-	"flag"
-	"fmt"
 	"github.com/miekg/dns"
 	"log"
 	"net/http"
-	"os"
 	"unbound"
 )
 
 func CheckHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Hello")
+	vars := mux.Vars(r)
+	zone := vars["domain"]
+	ds := vars["ds"]
+	log.Printf("%s: %s", zone, ds)
+
+	u := unbound.New()
+	defer u.Destroy()
+	if err := setupUnbound(u); err != nil {
+		log.Printf("error %s\n", err.Error())
+		return
+	}
+	// As for NS, so we can use these later on
+	res, err := u.Resolve(zone, dns.TypeNS, dns.ClassINET)
+	if err != nil {
+		log.Printf("error %s\n", err.Error())
+		return
+	}
+
+	if res.HaveData {
+		if res.Secure {
+			log.Printf("Result is secure\n")
+		} else if res.Bogus {
+			log.Printf("Result is bogus: %s\n", res.WhyBogus)
+		} else {
+			log.Printf("Result is insecure\n")
+		}
+	} else {
+		println("NO DATA")
+		return
+	}
+
+	u1 := unbound.New()
+	setupUnbound(u1)
+	if err := u1.AddTaFile(ds); err != nil {
+		log.Printf("error %s\n", err.Error())
+		return
+	}
+
+	res, err = u1.Resolve(zone, dns.TypeNS, dns.ClassINET)
+	if err != nil {
+		log.Printf("error %s\n", err.Error())
+		return
+	}
+	if res.HaveData {
+		if res.Secure {
+			log.Printf("Result is secure\n")
+		} else if res.Bogus {
+			log.Printf("Result is bogus: %s\n", res.WhyBogus)
+		} else {
+			log.Printf("Result is insecure\n")
+		}
+	} else {
+		log.Printf("NO DATA")
+		return
+	}
 }
 
 func main() {
@@ -22,60 +74,8 @@ func main() {
 	http.Handle("/", router)
 
 	e := http.ListenAndServe(":12345", nil)
-        if e != nil {
-        	log.Fatal("ListenAndServe: ", e)
-        }
-
-
-	u := unbound.New()
-	defer u.Destroy()
-	flag.Parse()
-	if err := setupUnbound(u); err != nil {
-		fmt.Printf("error %s\n", err.Error())
-		os.Exit(1)
-	}
-
-	// As for NS, so we can use these later on
-	r, err := u.Resolve(flag.Arg(0), dns.TypeNS, dns.ClassINET)
-	if err != nil {
-		fmt.Printf("error %s\n", err.Error())
-		os.Exit(1)
-	}
-	if r.HaveData {
-		if r.Secure {
-			fmt.Printf("Result is secure\n")
-		} else if r.Bogus {
-			fmt.Printf("Result is bogus: %s\n", r.WhyBogus)
-		} else {
-			fmt.Printf("Result is insecure\n")
-		}
-	} else {
-		println("NO DATA")
-		os.Exit(1)
-	}
-
-
-	u1 := unbound.New()
-	setupUnbound(u1)
-	if err := u1.AddTaFile(flag.Arg(1)); err != nil {
-		println(err.Error())
-	}
-	r, err = u1.Resolve(flag.Arg(0), dns.TypeNS, dns.ClassINET)
-	if err != nil {
-		fmt.Printf("error %s\n", err.Error())
-		os.Exit(1)
-	}
-	if r.HaveData {
-		if r.Secure {
-			fmt.Printf("Result is secure\n")
-		} else if r.Bogus {
-			fmt.Printf("Result is bogus: %s\n", r.WhyBogus)
-		} else {
-			fmt.Printf("Result is insecure\n")
-		}
-	} else {
-		println("NO DATA")
-		os.Exit(1)
+	if e != nil {
+		log.Fatal("ListenAndServe: ", e)
 	}
 
 }
