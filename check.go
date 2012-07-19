a44df997e906fbd743b85455b844e46c74d0dfeb
Miek Gieben <miek@miek.nl>
2012-07-19 12:01:27 +0200

    Style stuff

--- check.go
+++ check.go
@@ -12,6 +12,8 @@ import (
 	"unbound"
 )
 
+const LIMIT = 1000
+
 type result struct {
 	name   string // name to be checked
 	err    string // error from unbound (if any)
@@ -113,11 +115,18 @@ func parseHandlerCSV(w http.ResponseWriter, r *http.Request) {
 	o := csv.NewWriter(w)
 	record, err := v.Read()
 	all := NewAllResults()
+	i := 0
+Check:
 	for err == nil {
 		for _, r := range record {
 			result := unboundcheck(u, r)
 			log.Printf("%v\n", result)
 			all.Append(result)
+			i++
+			if i > LIMIT {
+				// Nu is het zat...!
+				break Check
+			}
 		}
 		record, err = v.Read()
 	}
@@ -134,15 +143,50 @@ func form(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, `
 <html>
 	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Portfolio check</title>
+	<link rel="shortcut icon" type="image/x-icon" href="http://sidnlabs.nl/favicon.ico">
+	<link href="http://www.sidnlabs.nl/fileadmin/sidnlabs/css/all.css" rel="stylesheet" charset="utf-8" type="text/css"/>
+	<style type="text/css" media="screen">
+.portfolio {
+	margin-left: auto;
+	margin-right; auto;
+	width: 80%;
+}
+dt {
+	font-weight: bold;
+}
+	</style>
 	</head>
 	<body>
-	<h1>Selecteer een <em>CSV</em> bestand met domein namen:</h1>
+	<div id="header">
+		<div class="holder">
+			<div class="logolink"><h1 class="logo"><a href="http://sidnlabs.nl">SIDN Labs een kijkje achter de schermen</a></h1></div>
+			<div class="navigation"><ul id="nav"><li><a href="http://www.sidnlabs.nl/over-sidn-labs/" onfocus="blurLink(this)">OVER SIDN LABS</a></li><li><a href="http://www.sidnlabs.nl/contact/" onfocus="blurLink(this)">CONTACT</a></li><li><a href="http://www.werkenbijsidn.nl/" onfocus="blurLink(this)">WERKEN BIJ SIDN</a></li><li><a href="http://www.sidn.nl" onfocus="blurLink(this)">NAAR SIDN.NL</a></li></ul></div>
+		</div>
+	</div>
+	<div id="wrapper">
+	<div id="canvas">
+	<p>&nbsp;</p>
+	<p>&nbsp;</p>
+	<p>&nbsp;</p>
+	<div class="portfolio">
+	<div class="pagetitle"><h1>SIDN Labs: Portfolio checker</h1></div>
+	<p>&nbsp;</p>
+	</div>
+
+	<div class="portfolio">
+	<div class="pagetitle"><h2>Selecteer een <em>CSV</em> bestand met domein namen</h2></div>
+	<p> 
 	<form action="http://localhost:8080/upload" method="POST" enctype="multipart/form-data">
 	<input type="file" name="domainlist">
 	<input type="submit" value="Controleer">
 	</form>
-	<h2>FAQ</h2>
+	</p>
+	</div>
+
+	<div class="portfolio">
+	<div class="pagetitle"><h2>FAQ</h2></div>
 	<dl>
 	<dt>Hoe wordt er gecontroleeerd?</dt>
 	<dd>U uploadt een CSV bestand met domein namen. Alle namen worden gecontroleerd, dus ook niet-.nl domein namen.
@@ -184,24 +228,30 @@ func form(w http.ResponseWriter, r *http.Request) {
 	<p>
 	De uitvoer daarvan is gelijk aan de portfolio-check uitvoer (CSV).
 	</p>
-	<dd>Welke software is gebruikt?</dd>
+	<dt>Welke software wordt gebruikt?</dt>
 	Deze portfolio-check gebruikt:
 	<ul>
 		<li>Libunbound van <a href="http://www.nlnetlabs.nl">NLnet Labs</a></li>
 		<li>De taal <a href="http://www.golang.org">Go</a></li>
 	</ul>
-	De complete software is te vinden op <a href="http://github.com/SIDN/unboundcheck>github.com/SIDN/unboundcheck</a>.
+	De software zelf is te vinden op <a href="http://github.com/SIDN/unboundcheck>github.com/SIDN/unboundcheck">github.com</a>.
 	</dd>
 	</dl>
-	<h2>Disclaimer</h2>
-	Dit is beta software! 
+	</div>
+
+	<div class="portfolio">
+	<div class="pagetitle"><h2>Disclaimer</h2></div>
+	Dit is beta software! Neem bij problemen met het gebruikt van deze software contact op met miek.gieben@sidn.nl .
+	</div>
+
+	</div> <!-- canvas -->
+	</div> <!-- wrapper -->
 	</body>
 </html>`)
 }
 
 func main() {
 	router := mux.NewRouter()
-	//router.HandleFunc("/precheck/{domain}/{anchor}", preCheckHandler) // TODO(mg)
 	router.HandleFunc("/check/{domain}", checkHandler)
 	router.HandleFunc("/upload", parseHandlerCSV)
 	router.HandleFunc("/form", form)
